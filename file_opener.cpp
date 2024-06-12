3a08a229d7db2762012b5a4f775a8e670feea7fd
sun-man-ram <manikantasunkara2653@gmail.com>
2024-06-12 10:27:16 +0530

    phase 1 of reading the input c++ file and copying its content and passign character stream

---
+++ file_opener.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc,char** argv){
+    char *fileOpen=argv[1];
+    FILE *filePointer;
+    filePointer=fopen(fileOpen,"r");
+    if(filePointer==NULL){
+        cout<<"failed to open file";
+        exit(1);
+    }
+    else{
+     fseek(filePointer,0,SEEK_END);
+     int fileSize=ftell(filePointer);
+    fseek(filePointer,0,SEEK_SET);
+    char *buffer=NULL;
+    buffer=new(nothrow) char[fileSize+1];
+    if(!buffer){
+        cout<<"allocation of memory failed";
+    }
+    else{
+    fread(buffer,sizeof(char),fileSize,filePointer);
+    cout<<"file opened successfully";
+    // scanner(buffer);
+    }
+    delete buffer;
+    fclose(filePointer);
+    }
+
+    return 0;
+}
